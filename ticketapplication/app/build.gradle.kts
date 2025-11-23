a61b47c68502278dc6ab69e3e38c1092a06cc0a0
scibior <szymon.scibior@epfl.ch>
2025-11-23 03:18:40 +0100

    nicer UI (i guess), some debugging, new firebase and app features

--- ticketapplication/app/build.gradle.kts
+++ ticketapplication/app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
 
     // --- FIREBASE DEPENDENCIES ---
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
